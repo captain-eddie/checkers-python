647dcd79b1caa6f8de2ade8e932c9f8d50205620
captain-eddie <edward.guobadia@gmail.com>
2022-10-15 23:26:04 -0400

    Stable branch
    Working on figuring out why each branch is a duplicate of each other, end goal is to make each element be the postion from 0 to last element.

--- main.py
+++ main.py
@@ -8,12 +8,13 @@ from checkers_class_info import gameboard
 import logging
 
 def main():
-    # set defualt logging level to DEBUG
+    #   set defualt logging level to DEBUG
     logging.basicConfig(level = logging.DEBUG)
-
+    
+    #   sets up game board
     rows, cols = int(input("Number of rows: ")), int(input("Number of cols: "))
     my_board = gameboard.Board(rows, cols)
-    logging.debug(f"\n{rows}x{cols} checkers game-board is ->\n")
+    #   logging.debug(f"\n{rows}x{cols} checkers game-board is ->\n")
     print(my_board)
 
 
